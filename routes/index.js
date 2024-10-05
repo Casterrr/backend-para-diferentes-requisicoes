7a2cd002d0ed98d7f10338d24f6c6a63cc9ccae9
Lucas Matheus <lucmvl2003@gmail.com>
2024-10-04 21:52:22 -0300

    feat: Atividade concluída

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,38 @@ router.get('/alunos', function(req, res, next) {
 router.post('/alunos', function(req, res, next) {
   console.log(req.body);
 
-  const mensagem = saveAluno(req.body);
+  const resultado = saveAluno(req.body);
 
-  res.status(201).json({ mensagem });
+  if (resultado.sucesso) {
+      res.status(201).json({ mensagem: resultado.mensagem });
+  } else {
+      res.status(400).json({ mensagem: resultado.mensagem });
+  }
+});
+
+router.put('/alunos/:matricula', function(req, res, next) {
+  const matricula = req.params.matricula;
+  const alunoAtualizado = req.body;
+
+  const resultado = updateAluno(matricula, alunoAtualizado);
+
+  if (resultado.sucesso) {
+      res.status(200).json({ mensagem: resultado.mensagem });
+  } else {
+      res.status(400).json({ mensagem: resultado.mensagem });
+  }
+});
+
+router.delete('/alunos/:matricula', function(req, res, next) {
+  const matricula = req.params.matricula;
+  
+  const resultado = deleteAluno(matricula);
+
+  if (resultado.sucesso) {
+    res.status(200).json({ mensagem: resultado.mensagem });
+  } else {
+    res.status(404).json({ mensagem: resultado.mensagem });
+  }
 });
 
 module.exports = router;
@@ -31,30 +60,50 @@ function getAlunos(){
     return alunos
 }
 
-function saveAluno(aluno){
-    console.log('opa')
+function saveAluno(aluno) {
+  if (temValoresNulos(aluno)) {
+      return { sucesso: false, mensagem: "Erro: Há campos nulos. Todos os campos são obrigatórios." };
+  }
 
-    if (alunos.find(alunoDaLista => alunoDaLista.matricula == aluno.matricula)) {
-      return "Já existe um aluno com essa matrícula."
-    }
-    
-    alunos.push(aluno);
-
-    return `Aluno ${aluno.nome} cadastrado com sucesso!`;
-
-
+  if (alunos.find(alunoDaLista => alunoDaLista.matricula == aluno.matricula)) {
+      return { sucesso: false, mensagem: "Já existe um aluno com essa matrícula." };
+  }
+  
+  alunos.push(aluno);
+  return { sucesso: true, mensagem: `Aluno ${aluno.nome} cadastrado com sucesso!` };
 }
 
-function updateAluno(aluno) {
-    alunos = alunos.map((alunoDaLista) => {
-        if (alunoDaLista.matricula == aluno.matricula) {
-            return aluno;
-        } else {
-            return alunoDaLista;
-        };
-    });
+function updateAluno(matricula, alunoAtualizado) {
+  if (temValoresNulos(alunoAtualizado)) {
+      return { sucesso: false, mensagem: "Erro: Há campos nulos. Todos os campos são obrigatórios." };
+  }
+
+  const index = alunos.findIndex(aluno => aluno.matricula === matricula);
+
+  if (index !== -1) {
+      alunos[index] = { ...alunos[index], ...alunoAtualizado };
+      return { sucesso: true, mensagem: "Aluno atualizado com sucesso." };
+  }
+  return { sucesso: false, mensagem: "Aluno com esta matrícula não encontrado." };
 }
 
-function deleteAluno(aluno) {
-    alunos.filter(alunoDaLista => alunoDaLista.matricula != aluno.matricula);
+function temValoresNulos(objeto) {
+  return Object.values(objeto).some(valor => valor === null || valor === undefined || valor === '');
+}
+
+function deleteAluno(matricula) {
+  const indexAluno = alunos.findIndex(aluno => aluno.matricula === matricula);
+  
+  if (indexAluno !== -1) {
+      alunos.splice(indexAluno, 1);
+      return { 
+          sucesso: true, 
+          mensagem: `Aluno com matrícula ${matricula} removido com sucesso.` 
+      };
+  } else {
+      return { 
+          sucesso: false, 
+          mensagem: `Aluno com matrícula ${matricula} não encontrado.` 
+      };
+  }
 }
